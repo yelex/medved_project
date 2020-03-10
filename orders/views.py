0b3a96546e68cf877f93ebd62976cbd7635e0031
Алексей Евсеев <user@MacBook-Pro-Aleksej.local>
2020-03-10 13:47:08 +0300

    first commit on new master

--- orders/views.py
+++ orders/views.py
@@ -9,35 +9,53 @@ def basket_adding(request):
     if not session_key:
         request.session.cycle_key()
 
-    print(request.POST)
+    # print(request.POST)
     data = request.POST
-    product_id = data.get("product_id")
-    nmb = data.get("nmb")
-    is_delete = bool(data.get("is_delete"))
+    # print('data: ', data)
+    product_id = int(data.get("product_id"))
+    nmb = int(data.get("nmb"))
+    is_delete = data.get("is_delete")
+    # print('product_id: {}\nnmb: {}\nis_delete: {}'.format(product_id, nmb, is_delete))
 
-    if is_delete:
-        ProductInBasket.objects.filter(session_key=session_key, product_id=product_id).update(is_active=False) #TODO 1.8.4 на 8:23
+    if is_delete == 'true':
+        print('im here')
+        # product_id - ProductInBasket id
+        ProductInBasket.objects.filter(id=product_id).update(is_active=False, nmb=nmb)  # product_id - ProductInBasket id
     else:
+        # product_id - ProductInBasket.product id
+        print('now im here')
         new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key,
-                                                                     product_id=product_id, defaults={"nmb": nmb})
+                                                                     product_id=product_id,
+                                                                     defaults={"nmb": nmb, "is_active": True})
+
         if not created:
+            new_product.is_active = True
+            # print('not created')
+            # print('int(new_product.nmb):', int(new_product.nmb))
+            # print('int(nmb):', int(nmb))
+            # print('new_product.is_active', new_product.is_active)
             new_product.nmb = int(new_product.nmb) + int(nmb)
             new_product.save(force_update=True)
+            print('after save new_product.nmb', new_product.nmb)
 
     # common code for 2 cases
     products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
-    return_dict["products_total_nmb"] = products_in_basket.count()
+    # print('products_in_basket', products_in_basket)
+    return_dict["products_in_basket_total_nmb"] = products_in_basket.count()
     return_dict["products"] = []
 
     for product_in_basket in products_in_basket:
         dict_product = dict()
-        dict_product["id"] = product_in_basket.product.id
+        dict_product["id"] = product_in_basket.id
         dict_product["name"] = product_in_basket.product.name
         dict_product["price_per_item"] = product_in_basket.price_per_item
         dict_product["nmb"] = product_in_basket.nmb
         return_dict["products"].append(dict_product)
 
     return JsonResponse(return_dict)
+    # return dict such as
+    # {'products_total_nmb': 123, {'id':1, 'name': 'product1', .., 'nmb':3}, ..}
+
 
 
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
     var form = $('#form-buying-product');
 
     function basketUpdating(product_id, nmb, is_delete){
+
         var data = {};
         data.product_id = product_id;
         data.nmb = nmb;
         var csrf_token = $('#form-buying-product [name="csrfmiddlewaretoken"]').val();
         data.csrfmiddlewaretoken = csrf_token;
+        data.is_delete = is_delete
 
         var url = form.attr("action");
         console.log(data);
@@ -19,17 +21,21 @@ $(document).ready(function(){
             success: function (data) {
 //                data, возвращаемая из views (basket_adding)
             console.log("OK");
-            console.log(data.products_total_nmb);
-            if (data.products_total_nmb){
-                $('#basket_total_nmb').text("(" + data.products_total_nmb + ")");
+//            console.log(data.products_in_basket_total_nmb);
+            $('#basket_total_nmb').text("(" + data.products_in_basket_total_nmb + ")");
+            $('.basket-items ul').empty();
+            if (data.products_in_basket_total_nmb!=0){
+//                $('#basket_total_nmb').text("(" + data.products_in_basket_total_nmb + ")");
                 console.log(data.products);
-                $('.basket-items ul').empty();
+//                $('.basket-items ul').empty();
                 $.each(data.products, function(k, v){
                 $('.basket-items ul').append('<li>' + v.name + ', ' + v.nmb + ' шт. по ' + v.price_per_item + 'руб.  '
-              + '<a href="" class="delete-item" data_product_id="' + v.id + '" data_nmb="' + v.nmb + '" is_delete="' + is_delete + '"'>x</a>'
+              + '<a href="" class="delete-item" data-product_id="' + v.id + '">x</a>'
                 + '</li>');
                 })
-            }
+            };
+
+            $('.basket-items').addClass('d-none');
 
             },
             error: function(){
@@ -43,39 +49,54 @@ $(document).ready(function(){
         var nmb = $('#number').val();
         var submit_btn = $('#submit-btn');
         var product_id = submit_btn.data('product_id');
-        var product_name = submit_btn.data('name');
-        var product_price =submit_btn.data('price');
-//        console.log(nmb);
+//        var product_name = submit_btn.data('name');
+//        var product_price =submit_btn.data('price');
+        console.log(nmb);
 //        console.log(product_id);
 //        console.log(product_name);
 //        console.log(product_price);
-        basketUpdating(form, product_id, nmb, is_delete=false);
+        basketUpdating(product_id, nmb, is_delete=false);
 
     });
 
 
     function showingBasket(){
-        $('.basket-items').toggleClass('d-none');
+        $('.basket-items').removeClass('d-none');
     }
 
-    $('.basket-container').click(function(e){
-        e.preventDefault();
-        showingBasket();
-    });
+//    $('.basket-container').click(function(e){
+//        e.preventDefault();
+//        showingBasket();
+//    });
 
     $('.basket-container').mouseover(function(){
-        showingBasket();
+        if ($('#basket_total_nmb').text()!='(0)'){
+            showingBasket();
+        }
+
+    });
+
+    $('.basket-container').hover(function(){
+//        console.log($('#basket_total_nmb').text()!='(0)');
+//        console.log($('#basket_total_nmb').text());
+        if ($('#basket_total_nmb').text()!='(0)'){
+                    showingBasket();
+                }
     });
 
     $('.basket-container').mouseout(function(){
-        showingBasket();
+//        console.log($('#basket_total_nmb').text());
+        if ($('#basket_total_nmb').text()!='(0)'){
+        $('.basket-items').addClass('d-none');
+        }
     });
 
     $(document).on('click', '.delete-item', function(e){
         e.preventDefault();
-        product_id = $(this).data("product_id")
+        product_id = $(this).data("product_id");
+        console.log('product_id ' + product_id);
+        nmb = 0;
 
         basketUpdating(product_id, nmb, is_delete=true);
-        $(this).closest('li').remove();
     });
 });
