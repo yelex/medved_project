d28ef8e5ce302ade0cc4da4b52f6665cf5309388
Алексей Евсеев <user@MacBook-Pro-Aleksej.local>
2020-03-14 15:14:08 +0300

    slider added

--- test_project/settings.py
+++ test_project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_prod/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod/')
 
 STATICFILES_DIRS = [
