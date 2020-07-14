ae340d858927364ac188c97f32602930937b6b2f
Alexey Evseev <evseev_alexey94@bk.ru>
2020-07-14 12:26:13 +0300

    prepared

--- test_project/settings_prod.py
+++ test_project/settings_prod.py
@@ -1,10 +1,10 @@
 DEBUG = False
 ALLOWED_HOSTS = ['134.122.82.169', 'localhost']
-print(DEBUG)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django-base',
+        'NAME': 'shop-base',
         'USER' : 'hello_django',
         'PASSWORD' : 'Fokina12',
         'HOST' : 'localhost',
