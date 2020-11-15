70a37f1e4c09d70c8fffe20a54869abb77aa4bd9
Alexey Evseev <evseev_alexey94@bk.ru>
2020-11-15 19:34:49 +0300

    +

--- test_project/settings.py
+++ test_project/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIAFILES_DIRS = [
     os.path.join(BASE_DIR, "media"),
 ]
